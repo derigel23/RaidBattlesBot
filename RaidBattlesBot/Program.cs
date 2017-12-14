ddcd78bbf604114d7865eb1a68d6d5fe42c1dd95
Slava Trenogin <slava.trenogin@jetbrains.com>
2017-12-14 22:04:34 +0300

    Baseline infrastructure

--- RaidBattlesBot/Program.cs
+++ RaidBattlesBot/Program.cs
@@ -5,14 +5,14 @@ namespace RaidBattlesBot
 {
   public class Program
   {
-    public static void Main(string[] args)
-    {
+    public static void Main(string[] args) =>
       BuildWebHost(args).Run();
-    }
 
     public static IWebHost BuildWebHost(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>()
-            .Build();
+      WebHost
+        .CreateDefaultBuilder(args)
+        .UseApplicationInsights()
+        .UseStartup<Startup>()
+        .Build();
   }
 }
