87d4bbdb607cb38c95ee0dcb4c023586711ea271
Slava Trenogin <slava.trenogin@jetbrains.com>
2018-01-06 10:22:35 +0300

    minor refactorings

--- RaidBattlesBot/StringEx.cs
+++ RaidBattlesBot/StringEx.cs
@@ -4,7 +4,7 @@ namespace RaidBattlesBot
 {
   public static class StringEx
   {
-    public static string JoinNonEmpty(string separator, params string[] values)
+    public static string JoinNonEmpty(this string separator, params string[] values)
     {
       return string.Join(separator, values.Where(_ => !string.IsNullOrEmpty(_)));
     }
