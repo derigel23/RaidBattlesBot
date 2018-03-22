4ae31f41f3e2d7ef52e8f5cbd4e372e00a599397
Slava Trenogin <slava.trenogin@jetbrains.com>
2018-03-22 20:55:34 +0300

    Support empty share command

--- RaidBattlesBot/Handlers/GeneralInlineQueryHandler.cs
+++ RaidBattlesBot/Handlers/GeneralInlineQueryHandler.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using DelegateDecompiler.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using NodaTime;
 using RaidBattlesBot.Model;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -21,16 +17,14 @@ namespace RaidBattlesBot.Handlers
     private readonly ITelegramBotClient myBot;
     private readonly IUrlHelper myUrlHelper;
     private readonly UserInfo myUserInfo;
-    private readonly RaidBattlesContext myDb;
-    private readonly IClock myClock;
+    private readonly ShareInlineQueryHandler myShareInlineQueryHandler;
 
-    public GeneralInlineQueryHandler(ITelegramBotClient bot, IUrlHelper urlHelper, UserInfo userInfo, RaidBattlesContext db, IClock clock)
+    public GeneralInlineQueryHandler(ITelegramBotClient bot, IUrlHelper urlHelper, UserInfo userInfo, ShareInlineQueryHandler shareInlineQueryHandler)
     {
       myBot = bot;
       myUrlHelper = urlHelper;
       myUserInfo = userInfo;
-      myDb = db;
-      myClock = clock;
+      myShareInlineQueryHandler = shareInlineQueryHandler;
     }
 
     public async Task<bool?> Handle(InlineQuery data, object context = default, CancellationToken cancellationToken = default)
@@ -40,22 +34,7 @@ namespace RaidBattlesBot.Handlers
       var query = data.Query;
       if (string.IsNullOrWhiteSpace(query))
       {
-        var userId = data.From.Id;
-        var now = myClock.GetCurrentInstant().ToDateTimeOffset();
-
-        // active raids or polls
-        var polls = await myDb.Polls
-          .IncludeRelatedData()
-          .Where(_ => _.EndTime > now) // live poll
-          .Where(_ => _.Raid.EggRaidId == null) // no eggs if boss is already known
-          .Where(_ => _.Owner == userId || _.Votes.Any(vote => vote.UserId == userId))
-          .OrderBy(_ => _.EndTime)
-          //.Take(10)
-          .DecompileAsync()
-          .ToArrayAsync(cancellationToken);
-
-        inlineQueryResults = await
-          Task.WhenAll(polls.Select(poll => poll.ClonePoll(myUrlHelper, myUserInfo, cancellationToken)));
+        inlineQueryResults = await myShareInlineQueryHandler.GetActivePolls(data.From, cancellationToken);
       }
       else
       {
--- RaidBattlesBot/Handlers/ShareInlineQueryHandler.cs
+++ RaidBattlesBot/Handlers/ShareInlineQueryHandler.cs
@@ -2,10 +2,12 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using DelegateDecompiler.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using NodaTime;
 using RaidBattlesBot.Model;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -21,57 +23,84 @@ namespace RaidBattlesBot.Handlers
     private readonly ITelegramBotClient myBot;
     private readonly IUrlHelper myUrlHelper;
     private readonly UserInfo myUserInfo;
+    private readonly IClock myClock;
 
-    public ShareInlineQueryHandler(RaidBattlesContext context, ITelegramBotClient bot, IUrlHelper urlHelper, UserInfo userInfo)
+    public ShareInlineQueryHandler(RaidBattlesContext context, ITelegramBotClient bot, IUrlHelper urlHelper, UserInfo userInfo, IClock clock)
     {
       myContext = context;
       myBot = bot;
       myUrlHelper = urlHelper;
       myUserInfo = userInfo;
+      myClock = clock;
     }
 
-    public async Task<bool?> Handle(InlineQuery data, object context = default, CancellationToken cancellationToken = default)
+    async Task<bool?> IHandler<InlineQuery, object, bool?>.Handle(InlineQuery data, object context = default, CancellationToken cancellationToken = default)
     {
       var queryParts = data.Query.Split(':');
       if (queryParts[0] != "share")
         return null;
 
+      ICollection<InlineQueryResult> inlineQueryResults;
       if (!int.TryParse(queryParts.ElementAtOrDefault(1) ?? "", out var pollid))
-        return false;
-
-      var poll = await myContext.Polls
-        .Where(_ => _.Id == pollid)
-        .IncludeRelatedData()
-        .FirstOrDefaultAsync(cancellationToken);
-
-      var inlineQueryResults = new List<InlineQueryResult>();
-      if (poll != null)
       {
-        inlineQueryResults.Add(await poll.ClonePoll(myUrlHelper, myUserInfo, cancellationToken));
+        inlineQueryResults = await GetActivePolls(data.From, cancellationToken);
+      }
+      else
+      {
+        var poll = await myContext.Polls
+          .Where(_ => _.Id == pollid)
+          .IncludeRelatedData()
+          .FirstOrDefaultAsync(cancellationToken);
 
-        if (poll.Raid() is Raid raid)
+        inlineQueryResults = new List<InlineQueryResult>();
+        if (poll != null)
         {
-          inlineQueryResults.Add(
-            new InlineQueryResultVenue
-            {
-              Id = $"location:{raid.Id}",
-              Title = raid.Title,
-              Address = "Запостить локу",
-              Latitude = (float)raid.Lat,
-              Longitude = (float)raid.Lon,
-              ThumbUrl = myUrlHelper.AssetsContent("static_assets/png/ic_map.png").ToString(),
-              InputMessageContent = new InputVenueMessageContentNew
+          inlineQueryResults.Add(await poll.ClonePoll(myUrlHelper, myUserInfo, cancellationToken));
+
+          if (poll.Raid() is Raid raid)
+          {
+            inlineQueryResults.Add(
+              new InlineQueryResultVenue
               {
-                Name = raid.Title,
-                Address = RaidEx.Delimeter.JoinNonEmpty(raid.Gym ?? raid.PossibleGym, raid.Description),
-                Latitude = (float)raid.Lat,
-                Longitude = (float)raid.Lon,
-              },
-            });
+                Id = $"location:{raid.Id}",
+                Title = raid.Title,
+                Address = "Запостить локу",
+                Latitude = (float) raid.Lat,
+                Longitude = (float) raid.Lon,
+                ThumbUrl = myUrlHelper.AssetsContent("static_assets/png/ic_map.png").ToString(),
+                InputMessageContent = new InputVenueMessageContentNew
+                {
+                  Name = raid.Title,
+                  Address = RaidEx.Delimeter.JoinNonEmpty(raid.Gym ?? raid.PossibleGym, raid.Description),
+                  Latitude = (float) raid.Lat,
+                  Longitude = (float) raid.Lon,
+                },
+              });
+          }
         }
       }
 
       return await myBot.AnswerInlineQueryAsync(data.Id, inlineQueryResults.ToArray(), cacheTime: 0, cancellationToken: cancellationToken);
+    }
+
+    public async Task<InlineQueryResultArticle[]> GetActivePolls(User user, CancellationToken cancellationToken = default)
+    {
+      var userId = user.Id;
+      var now = myClock.GetCurrentInstant().ToDateTimeOffset();
+
+      // active raids or polls
+      var polls = await myContext.Polls
+        .IncludeRelatedData()
+        .Where(_ => _.EndTime > now) // live poll
+        .Where(_ => _.Raid.EggRaidId == null) // no eggs if boss is already known
+        .Where(_ => _.Owner == userId || _.Votes.Any(vote => vote.UserId == userId))
+        .OrderBy(_ => _.EndTime)
+        //.Take(10)
+        .DecompileAsync()
+        .ToArrayAsync(cancellationToken);
+
+      return await
+        Task.WhenAll(polls.Select(poll => poll.ClonePoll(myUrlHelper, myUserInfo, cancellationToken)));
     }
 
     [JsonObject(Title = "InputVenueMessageContent", MemberSerialization = MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
