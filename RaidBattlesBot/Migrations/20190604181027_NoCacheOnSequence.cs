6ed8a965b2a5083afa1c366a378b2dca4a7a27ef
Slava Trenogin <slava.trenogin@jetbrains.com>
2019-06-04 21:14:10 +0300

    No cache on sequence

---
+++ RaidBattlesBot/Migrations/20190604181027_NoCacheOnSequence.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RaidBattlesBot.Migrations
+{
+    public partial class NoCacheOnSequence : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER SEQUENCE PollId NO CYCLE NO CACHE; ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
