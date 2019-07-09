318812a91ea47c446a3231b283cc77a544ab6c28
Slava Trenogin <slava.trenogin@jetbrains.com>
2019-07-09 19:29:31 +0300

    Update poll counter seed

---
+++ RaidBattlesBot/Migrations/20190709162632_PollIdSeedChanged.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RaidBattlesBot.Migrations
+{
+    public partial class PollIdSeedChanged : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RestartSequence(
+                name: "PollId",
+                startValue: 10000000L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RestartSequence(
+                name: "PollId",
+                startValue: 100000L);
+        }
+    }
+}
