b9620fb460ce9bfda061e041758ab6b6740b59dc
Slava Trenogin <slava.trenogin@jetbrains.com>
2020-05-04 15:26:26 +0300

    More status info

--- RaidBattlesBot/Handlers/StatusProvider.cs
+++ RaidBattlesBot/Handlers/StatusProvider.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RaidBattlesBot.Model;
 using Team23.TelegramSkeleton;
 
 namespace RaidBattlesBot.Handlers
@@ -9,16 +12,20 @@ namespace RaidBattlesBot.Handlers
   public class StatusProvider : IStatusProvider
   {
     private readonly IUrlHelper myUrlHelper;
+    private readonly RaidBattlesContext myDB;
 
-    public StatusProvider(IUrlHelper urlHelper)
+    public StatusProvider(IUrlHelper urlHelper, RaidBattlesContext db)
     {
       myUrlHelper = urlHelper;
+      myDB = db;
     }
     
-    public Task<IDictionary<string, object>> Handle(IDictionary<string, object> status, ControllerContext context, CancellationToken cancellationToken = default)
+    public async Task<IDictionary<string, object>> Handle(IDictionary<string, object> status, ControllerContext context, CancellationToken cancellationToken = default)
     {
       status["assetsRoot"] = myUrlHelper.AssetsContent("~");
-      return Task.FromResult(status);
+      status["lastAppliedMigration"] = (await myDB.Database.GetAppliedMigrationsAsync(cancellationToken)).LastOrDefault();
+      status["polls"] = await myDB.Set<Poll>().LongCountAsync(cancellationToken);
+      return status;
     }
   }
 }
