4ad123f380d379f423136b1e087accb1cd48785d
Slava Trenogin <slava.trenogin@jetbrains.com>
2020-10-04 13:38:45 +0300

    SHow timezone info in status

--- RaidBattlesBot/Handlers/StatusProvider.cs
+++ RaidBattlesBot/Handlers/StatusProvider.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NodaTime;
 using RaidBattlesBot.Model;
 using Team23.TelegramSkeleton;
 
@@ -13,15 +14,20 @@ namespace RaidBattlesBot.Handlers
   {
     private readonly IUrlHelper myUrlHelper;
     private readonly RaidBattlesContext myDB;
+    private readonly DateTimeZone myDateTimeZone;
+    private readonly IClock myClock;
 
-    public StatusProvider(IUrlHelper urlHelper, RaidBattlesContext db)
+    public StatusProvider(IUrlHelper urlHelper, RaidBattlesContext db, DateTimeZone dateTimeZone, IClock clock)
     {
       myUrlHelper = urlHelper;
       myDB = db;
+      myDateTimeZone = dateTimeZone;
+      myClock = clock;
     }
     
     public async Task<IDictionary<string, object>> Handle(IDictionary<string, object> status, ControllerContext context, CancellationToken cancellationToken = default)
     {
+      status["defaultTimeZone"] = $"{myDateTimeZone.Id}, {myDateTimeZone.GetZoneInterval(myClock.GetCurrentInstant())}";
       status["assetsRoot"] = myUrlHelper.AssetsContent("~");
       status["lastAppliedMigration"] = (await myDB.Database.GetAppliedMigrationsAsync(cancellationToken)).LastOrDefault();
       status["polls"] = await myDB.Set<Poll>().LongCountAsync(cancellationToken);
