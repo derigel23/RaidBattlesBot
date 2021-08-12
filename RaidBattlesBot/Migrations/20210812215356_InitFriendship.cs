f9765ca72d44c63fcb1078c054abd6f772d642ed
Slava Trenogin <slava.trenogin@jetbrains.com>
2021-08-13 01:06:44 +0300

    Initialize friendship data from previous polls

---
+++ RaidBattlesBot/Migrations/20210812215356_InitFriendship.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RaidBattlesBot.Migrations
+{
+    public partial class InitFriendship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          migrationBuilder.Sql(@$"
+INSERT INTO Friendship(Id, FriendId, Type, Modified)
+SELECT DISTINCT 
+       IIF(v1.UserId <= v2.UserId, v1.UserId, v2.UserId) AS Id,
+       IIF(v1.UserId <= v2.UserId, v2.UserId, v1.UserId) AS FriendId,
+       1 As Type,
+       GETDATE() AS Modified
+FROM Votes v1, Votes v2
+WHERE v1.PollId = v2.PollId AND
+      v1.Team & {Model.VoteEnum.Yes | Model.VoteEnum.Remotely:D} != 0 AND
+      v2.Team & {Model.VoteEnum.Invitation:D} != 0");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          migrationBuilder.Sql("DELETE * FROM Friendship");
+        }
+    }
+}
