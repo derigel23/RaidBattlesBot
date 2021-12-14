d90c4741e99556963c199dad5dd0f5f2d0ffbb18
Slava Trenogin <slava.trenogin@jetbrains.com>
2021-12-15 00:17:27 +0300

    Notification Channels fix 2

---
+++ RaidBattlesBot/Migrations/20211214211658_NotificationChannelsFix2.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RaidBattlesBot.Migrations
+{
+    public partial class NotificationChannelsFix2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ReplyNotifications",
+                table: "ReplyNotifications");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BotId",
+                table: "ReplyNotifications",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ReplyNotifications",
+                table: "ReplyNotifications",
+                columns: new[] { "ChatId", "FromChatId", "FromMessageId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ReplyNotifications",
+                table: "ReplyNotifications");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "BotId",
+                table: "ReplyNotifications",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ReplyNotifications",
+                table: "ReplyNotifications",
+                columns: new[] { "BotId", "ChatId", "FromChatId", "FromMessageId" });
+        }
+    }
+}
