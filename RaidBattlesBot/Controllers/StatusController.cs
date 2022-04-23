6f0a6f19df5656448ce0d2a8cef5450554a55850
Slava Trenogin <derigel@gmail.com>
2022-04-23 10:06:54 +0300

    IWebHookSaltProvider is optional

--- RaidBattlesBot/Controllers/StatusController.cs
+++ RaidBattlesBot/Controllers/StatusController.cs
@@ -11,8 +11,8 @@ namespace RaidBattlesBot.Controllers
 {
   public class StatusController : StatusController<PollMessage, bool?, BotCommandAttribute>
   {
-    public StatusController(IWebHookSaltProvider? webHookSaltProvider, IEnumerable<ITelegramBotClient> bots, IEnumerable<IStatusProvider> statusProviders,
-      IEnumerable<Lazy<Func<Message, IBotCommandHandler<PollMessage, bool?>>, BotCommandAttribute>> commandHandlers)
+    public StatusController(IEnumerable<ITelegramBotClient> bots, IEnumerable<IStatusProvider> statusProviders,
+      IEnumerable<Lazy<Func<Message, IBotCommandHandler<PollMessage, bool?>>, BotCommandAttribute>> commandHandlers, IWebHookSaltProvider? webHookSaltProvider = null)
       : base(webHookSaltProvider, bots, statusProviders, commandHandlers)
     {
     }
